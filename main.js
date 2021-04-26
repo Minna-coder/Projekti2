f2154bf84fef2822afc551108da22b58cf86fa15
Minna Mikkola <minna.mikkola@kotikone.f>
2021-04-26 12:00:05 +0300

    muutoksia

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+function printJSONTable(jsonObj) {
 
+	var data = jsonObj;
+	var out = "<table>";
+
+		out += '<tr>';
+		out += '<td>' + data.all + '<td>';
+		out += '</tr>';
+
+
+	out+="</table>";
+
+	document.getElementById("tabledata").innerHTML = out;
+}
