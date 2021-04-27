149787572b21f2e0035a92c029e1ab10d947a72f
Minna Mikkola <minna.mikkola@kotikone.f>
2021-04-28 01:19:47 +0300

    muokattu

---
+++ Objektit.js
@@ -0,0 +1,45 @@
+[{
+      "ID": "9db5e016-8f44-4eab-bdab-163291269d72",
+      "Country": "Finland",
+      "CountryCode": "FI",
+      "Slug": "finland",
+      "NewConfirmed": 0,
+      "TotalConfirmed": 85937,
+      "NewDeaths": 0,
+      "TotalDeaths": 906,
+      "NewRecovered": 0,
+      "TotalRecovered": 46000,
+      "Date": "2021-04-27T18:59:30.908Z",
+      "Premium": {}
+    },
+
+{
+      "ID": "47fd4d8a-a550-421c-8546-ba9dff4eab54",
+      "Country": "Sweden",
+      "CountryCode": "SE",
+      "Slug": "sweden",
+      "NewConfirmed": 0,
+      "TotalConfirmed": 938343,
+      "NewDeaths": 0,
+      "TotalDeaths": 13923,
+      "NewRecovered": 0,
+      "TotalRecovered": 0,
+      "Date": "2021-04-27T18:59:30.908Z",
+      "Premium": {}
+    },
+
+{
+      "ID": "a17e9ecc-962f-439b-b2e3-f3ec46bad7c2",
+      "Country": "Russian Federation",
+      "CountryCode": "RU",
+      "Slug": "russia",
+      "NewConfirmed": 8681,
+      "TotalConfirmed": 4717321,
+      "NewDeaths": 349,
+      "TotalDeaths": 106783,
+      "NewRecovered": 6621,
+      "TotalRecovered": 4344735,
+      "Date": "2021-04-27T18:59:30.908Z",
+      "Premium": {}
+    }
+]
---
+++ harjoitusjavas.js
@@ -0,0 +1,28 @@
+let url ="https://api.covid19api.com/summary";
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
+
+// Ajetaan kun vastaus saadaan
+xmlhttp.onreadystatechange=function() {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200){
+    // Muutetaan raakadata JSON muotoon
+    let data = JSON.parse( xmlhttp.responseText );
+    // Kaivetaan Suomen data esiin
+         console.log ( data.Countries[58] );
+    // Kaivetaan kenttäkohtaista dataa
+        console.log ( data.Countries[58].TotalConfirmed );
+        console.log ( data.Countries[58].TotalRecovered );
+        console.log ( data.Countries [58].Country );
+
+
+        document.write ( data.Countries [58].Country );
+
+        document.write ( data.Countries [58].TotalConfirmed );
+        
+        document.write ( data.Countries [58].TotalRecovered );
+        
+    }
+}
+
